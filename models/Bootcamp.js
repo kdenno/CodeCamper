e52339e612566028ca23f1e24fbc20aacff53f62
kdenno <kabuyedennis3@gmail.com>
2020-03-31 11:29:47 +0300

    create slugs with mongoose middleware and slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify"); 
 const bootcampSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -96,5 +97,11 @@ const bootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+// create Bootcamp slug before save is triggered
+bootcampSchema.pre('save', function(next){
+  // in here we have access to all the fields
+  this.slug = slugify(this.name, {lowercase: true});
+  next();
 });
 module.exports = mongoose.model("Bootcamp", bootcampSchema);
