c73a6b2c5d5860e0eba1385786c3bf49847c66a0
kdenno <kabuyedennis3@gmail.com>
2020-04-02 23:49:53 +0300

    add course to a specific bootcamp

--- controllers/coursecontrollers.js
+++ controllers/coursecontrollers.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const Course = require("../models/Courses");
 const asyncHandler = require("../middleware/asyncHandler");
+const ErrorMessage = require("../util/ErrorMessage");
+const Bootcamp = require("../models/Bootcamp");
 
 exports.getCourses = asyncHandler(async (req, res, next) => {
   let query;
@@ -14,4 +16,36 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   }
   const courses = await query;
   res.status(200).json({ success: true, count: courses.length, data: courses });
+});
+
+exports.getCourse = asyncHandler(async (req, res, next)=> {
+    if(!req.params.Id) {
+       next(new ErrorMessage('No Id found', 400));
+    }
+    const course =  await Course.findById(req.params.Id).populate({ path: 'bootcamp', select: 'name description'});
+    if(!course){
+        next(new ErrorMessage(`Course with ID ${req.params.Id} was not found`, 400));
+       
+    }
+    res.status(200).json({ success: true, data: course });
+
+
+});
+
+// @desc Add Course
+// @route POST /api/v1/bootcamps/:bootcampId/course--course is associated with a bootcamp
+// @access Private
+
+exports.addCourse = asyncHandler(async (req, res, next)=> {
+   // add bootcamp parameter to body the parameter should be the same as the foreign key in the Course model
+   req.body.bootcamp = req.params.bootcampId;
+    const bootcamp =  await Bootcamp.findById(req.params.bootcampId);
+    if(!bootcamp){
+        next(new ErrorMessage(`Bootcamp with ID ${req.params.bootcampId} was not found`, 400));
+       
+    }
+    const course = await Course.create(req.body);
+    res.status(200).json({ success: true, data: course });
+
+
 });
--- routes/Courseroutes.js
+++ routes/Courseroutes.js
@@ -2,5 +2,7 @@ const express = require("express");
 const routes = express.Router({mergeParams: true});
 const courseControllers = require("../controllers/coursecontrollers");
 
-routes.get("/", courseControllers.getCourses);
+routes.get("/", courseControllers.getCourses); // handles forwarded route with get /:bootcampId/courses
+routes.get("/:Id", courseControllers.getCourse);
+routes.post('/', courseControllers.addCourse); // handles forwarded route with post /:bootcampId/courses
 module.exports = routes;
