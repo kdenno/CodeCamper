e9b96d4a0a673523f1216015bf0bfc8629ba7cdd
kdenno <kabuyedennis3@gmail.com>
2020-04-04 14:29:06 +0300

    add login functionality

--- controllers/authcontrollers.js
+++ controllers/authcontrollers.js
@@ -4,7 +4,7 @@ const asyncHandler = require("../middleware/asyncHandler");
 
 // @desc Register user
 // @route POST /api/v1/auth/register
-// @access Private
+// @access Public
 
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body;
@@ -16,6 +16,31 @@ exports.register = asyncHandler(async (req, res, next) => {
   });
   // get the token off the user object
   const token = user.getSignedJwtToken();
-  
+
+  res.status(200).json({ success: true, token });
+});
+
+// @desc Login
+// @route POST /api/v1/auth/login
+// @access Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  // validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide email and password", 400));
+  }
+  const user = await User.findOne({ email }).select("+password"); // since we want the password for validation so we overide the select that we set in the model
+  if (!user) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+  // verify password
+  if (!(await user.matchPassword(password))) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+
+  // get the token off the user object
+  const token = user.getSignedJwtToken();
+
   res.status(200).json({ success: true, token });
 });
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
-const {register} = require("../controllers/authcontrollers");
+const { register, login } = require("../controllers/authcontrollers");
 
-routes.post('/register', register);
+routes.post("/register", register);
+routes.post("/login", login);
 
 module.exports = routes;
