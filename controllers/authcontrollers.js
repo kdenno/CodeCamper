afc35b386c8c1ea81d586c74a9aeb63b9aab0286
kdenno <kabuyedennis3@gmail.com>
2020-04-04 12:06:23 +0300

    password encryption

--- controllers/authcontrollers.js
+++ controllers/authcontrollers.js
@@ -7,5 +7,12 @@ const asyncHandler = require("../middleware/asyncHandler");
 // @access Private
 
 exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
   res.status(200).json({ success: true });
 });
